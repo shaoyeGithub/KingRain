8a19ce7e9bc60e606e3c0c60532e589e5b8438b2
shaoyeGithub <2636577435@qq.com>
2019-03-31 10:51:17 +0800

    update

--- kingRain/src/main.js
+++ kingRain/src/main.js
@@ -7,8 +7,9 @@ import router from './router'
 import 'element-ui/lib/theme-chalk/index.css'
 import axios from 'axios'
 
-// axios.defaults.baseURL = 'http://localhost:8080/'
-axios.defaults.baseURL = 'http://192.168.31.182:8080/'
+// axios.defaults.baseURL = 'http://localhost:8888/'
+// axios.defaults.baseURL = 'http://192.168.31.182:8080/'
+axios.defaults.baseURL = 'http://172.19.159.222:8888/'
 Vue.prototype.$axios = axios
 Vue.use(ElementUI)
 Vue.config.productionTip = false
